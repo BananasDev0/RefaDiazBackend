7e38e36f21d464fb5e101a7ba352a422326e1ab7
Jacob Calvillo <jakeob99@gmail.com>
2024-02-23 14:04:35 -0600

    crud brand

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,10 +3,30 @@ import Brand from "../models/brand.js";
 const getAll = async (req, res) => {
     try {
         const brands = await Brand.findAll(); // Recupera todas las marcas de la base de datos
-        res.send(brands); // Envía todas las marcas como respuesta
+        res.status(200).send(brands); // Envía todas las marcas como respuesta
     } catch (error) {
         console.error('Error al recuperar las marcas:', error);
         res.status(500).send('Error interno del servidor');
+    }
+}
+const getBrand = async(req, res) => {
+    try {
+        const brandId = req.params.id;
+        const brand = await Brand.findOne({ 
+            where: { 
+                id: brandId 
+            } 
+        });
+       
+        if(!brand) {
+            res.status(404).send('No existe esa Brand');
+            console.log("no existe");
+        } else {
+            res.status(200).send(brand);    
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
     }
 }
 
@@ -20,4 +40,43 @@ const createNewBrand = async(req, res) => {
     }
 }
 
-export { createNewBrand, getAll };
+const updateBrand = async(req, res) => {
+    try {
+        const brandId = req.params.id;
+        const updatedData = req.body;
+
+        const brand = Brand.findByPk(brandId);
+
+        if(!brand) {
+            res.status(404).send("No existe tal brand");
+        } else {
+            await brand.update(updatedData);
+            res.status(200).send(brand);
+        }
+
+    } catch (error) {
+        console.error('Error al actualizar la marca:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+}
+
+
+const deleteBrand = async(req, res) => {
+    try {
+        const brandId = req.params.id;
+        const brand = await Brand.findByPk(brandId)
+
+        if(!brand) {
+            res.status(404).send('brand not found');
+        } else {
+            await brand.destroy();
+            res.status(204).send();
+        }
+
+    } catch (error) {
+        console.log('Error al borrar brand: ', error);
+        res.status(500).send(error);
+    }
+}
+
+export { createNewBrand, getAll, deleteBrand, getBrand, updateBrand };
--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import { createNewBrand, getAll } from '../controllers/brandController.js';
+import { createNewBrand, getAll, getBrand, deleteBrand } from '../controllers/brandController.js';
 
 const brandRouter = express.Router();
 
 brandRouter.get('/brands', getAll);
+brandRouter.get('/brand', getBrand);
 
 brandRouter.post('/brand', createNewBrand);
+
+router.put('/brands/:id', updateBrand);
+
+brandRouter.delete('/brand/:id', deleteBrand);
 
 // Exporta el router
 export default brandRouter;
