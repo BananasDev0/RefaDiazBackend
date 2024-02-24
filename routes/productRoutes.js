a9204f29e7bd075ab9a0d05ff8783b6d105d8dfa
Jacob Calvillo <jakeob99@gmail.com>
2024-02-23 18:26:48 -0600

    client routes finished

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,20 @@
 import express from 'express';
-
+import { getAll, getProduct, deleteProduct, createProduct, updateProduct } from '../controllers/productController';
 
 // Crea una instancia del router de Express
 const productRouter = express.Router();
 
 // Define la ruta para obtener todos los productos y asocia el controlador
 // productRouter.get('/products', getAllProducts);
+
+productRouter.get('/products', getAll);
+productRouter.get('/product/:id', getProduct);
+
+productRouter.post('products', createProduct);
+
+productRouter.put('/product/:id', updateProduct);
+
+productRouter.delete('/product/:id', deleteProduct);
 
 // Exporta el router
 export default productRouter;
