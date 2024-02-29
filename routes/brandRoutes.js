91753ea0e65f6aa5449501ffb5023d920d3b5f18
Jacob Calvillo <jakeob99@gmail.com>
2024-02-29 16:56:19 -0600

    using middleware in routes

--- routes/brandRoutes.js
+++ routes/brandRoutes.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import { createNewBrand, getAll, getBrand, deleteBrand, updateBrand } from '../controllers/brandController.js';
+import firebaseTokenVerification from '../middleware/auth.js';
 
 const brandRouter = express.Router();
 
-brandRouter.get('/brands', getAll);
-brandRouter.get('/brand/:id', getBrand);
+brandRouter.get('/brands', firebaseTokenVerification, getAll);
+brandRouter.get('/brand/:id', firebaseTokenVerification, getBrand);
 
-brandRouter.post('/brand', createNewBrand);
+brandRouter.post('/brand', firebaseTokenVerification, createNewBrand);
 
-brandRouter.put('/brand/:id', updateBrand);
+brandRouter.put('/brand/:id', firebaseTokenVerification, updateBrand);
 
-brandRouter.delete('/brand/:id', deleteBrand);
+brandRouter.delete('/brand/:id', firebaseTokenVerification, deleteBrand);
 
 // Exporta el router
 export default brandRouter;
