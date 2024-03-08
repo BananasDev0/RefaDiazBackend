23b382652c5b20b11e70bfa33f9b073ee7ba3d69
Jacob Calvillo <jakeob99@gmail.com>
2024-03-07 22:55:04 -0600

    finishing routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import clientRouter from "./clientRoutes.js";
 import productRouter from "./productRoutes.js";
 import brandRouter from './brandRoutes.js';
+import personRouter from "./personRoutes.js";
+import userRouter from "./userRoutes.js";
 
 function buildRouter(app) {
     app.use('/api', productRouter);
     app.use('/api', clientRouter);
     app.use('/api', brandRouter);
+    app.use('/api', personRouter);
+    app.use('/api', userRouter);
 }
 
 export default buildRouter
