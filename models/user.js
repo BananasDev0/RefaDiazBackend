aff6b1b7f3eee637753e189f1d2fdb560602fb8a
Jacob Calvillo <jakeob99@gmail.com>
2024-03-07 22:36:40 -0600

    adding user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 import { Sequelize, DataTypes } from "sequelize";
 import sequelize from '../config/dbConnection.js';
+import Person from './person.js'; // Import the Person model
 
-class User extends Sequelize.Model{};
+class User extends Sequelize.Model {}
 
 User.init(
     {
@@ -10,6 +11,19 @@ User.init(
             autoIncrement: true,
             primaryKey: true
         },
-        
+        person_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'User',
+        tableName: 'user',
+        timestamps: false
     }
-)
+);
+
+User.belongsTo(Person, { foreignKey: 'person_id' }); 
+
+export default User;
