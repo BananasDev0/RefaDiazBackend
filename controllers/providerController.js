5a5a336859fd6b98edf890136d02f0a91f7df669
Jacob Calvillo <jakeob99@gmail.com>
2024-04-18 12:44:45 -0600

    modified getAll provider

--- controllers/providerController.js
+++ controllers/providerController.js
@@ -1,33 +1,27 @@
 import Provider from '../models/provider.js';
 
-const getAll = async(req, res) => {
-    try{
-        const { page=1, limit=10 } = req.query;
+const getAll = async (req, res) => {
+    try {
+        const { page, limit } = req.query;
 
         const offset = (parseInt(page) - 1) * parseInt(limit);
-        
-       
         const totalCount = await Provider.count();
 
         if (offset >= totalCount) {
             return res.status(200).send({ message: "No hay más resultados disponibles." });
         }
         
-       
         const providers = await Provider.findAll({
             offset: offset,
             limit: parseInt(limit)
-        });
+        });        
 
-
-        ///const providers = await Provider.findAll();
-        
-
-        res.status(200).send(providers);
-    } catch(error) {
+        res.status(200).send({ providers: providers, totalCount: totalCount });
+    } catch (error) {
         res.status(500).send(error.message);
     }
-}
+};
+
 
 const getProvider = async(req, res) => {
     try{
