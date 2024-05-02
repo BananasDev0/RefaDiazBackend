2f63af5c903e5e645e9464d7f4d8c08f3a9ec745
Roel Mendoza <roelmdz@gmail.com>
2024-05-02 14:27:24 -0600

    not needed id

--- scripts/db.sql
+++ scripts/db.sql
@@ -93,7 +93,6 @@ CREATE TABLE product_price(
 ) INHERITS (control_fields);
 
 CREATE TABLE provider_product(
-    id SERIAL PRIMARY KEY,
     product_id INT NOT NULL,
     price_id INT NOT NULL,
     provider_id INT NOT NULL,
